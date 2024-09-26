0964e4e743036c03603b415fb71e859261a6f197
Harshana Batagalla <harshanabatagalla@gmail.com>
2024-09-26 16:35:21 +0530

    Changed theme to dim

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,17 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      fontFamily: {
+        poppins: ['Poppins', 'sans-serif'],
+      },
+      container: {
+        padding: {
+          DEFAULT: '2rem',
+          sm: '3rem',
+          lg: '5rem',
+          xl: '6rem',
+          '2xl': '7rem',
+        },
       },
     },
   },
@@ -20,7 +27,7 @@ const config: Config = {
     require('daisyui'),
   ],
   daisyui: {
-    themes: ["cupcake","bumblebee"],
+    themes: ["cupcake","bumblebee","dark","dim"],
   },
 }
 export default config

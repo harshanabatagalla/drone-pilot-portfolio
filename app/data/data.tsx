b21a6761a97216c957c96d428b82dbd18ffa844d
Harshana Batagalla <harshanabatagalla@gmail.com>
2024-09-27 13:54:27 +0530

    Added data file for dynamic data

---
+++ app/data/data.tsx
@@ -0,0 +1,8 @@
+const social_media_links = {
+    facebook : "https://www.facebook.com/sankalpabandara01",
+    instagram : "https://www.instagram.com/sankalpa_bandara_/",
+    tiktok : "https://www.tiktok.com/@sankalpa_bandara",
+    whatsapp: ""
+}
+
+export  {social_media_links } ;

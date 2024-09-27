5e1b14d08ba771726080969a9f67eaa95605b2d3
Harshana Batagalla <harshanabatagalla@gmail.com>
2024-09-27 11:08:47 +0530

    implemented hero section

---
+++ app/components/Hero.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import styles from './Hero.module.css'
+
+const Hero = () => {
+  return (
+    <section className='h-[90vh] shrink-0 flex flex-col items-center justify-center gap-6 relative'>
+  <video
+    className='absolute w-full h-full object-cover -z-10 brightness-50'
+    muted
+    autoPlay
+    loop
+    playsInline
+    disablePictureInPicture
+  >
+    <source src='assets/videos/hero_background.mp4' type='video/mp4' />
+  </video>
+  <div className='container'>
+    <h1 className='text-3xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl uppercase font-extrabold'>
+      See the World from <br/>My <span className='text-primary'>Lenses</span>
+    </h1>
+    
+    <p className='text-sm sm:text-base md:text-sm lg:text-lg xl:text-xl font-extralight mt-4 sm:w-4/5 w-full'>
+      From breathtaking cinematic views to accurate inspections, I bring the sky’s perspective to your fingertips. Ready to transform your project with aerial expertise? Let's take your ideas to new horizons.
+    </p>
+    
+    <button className='btn btn-primary mt-6 '>Book Your Flight Now</button>
+  </div>
+</section>
+
+  )
+}
+
+export default Hero

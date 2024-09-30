379f4adfcbb0ca406114a3c22a7c32148c3094c0
Harshana Batagalla <67497499+harshanabatagalla@users.noreply.github.com>
2024-09-30 19:02:47 +0530

    Added about me card data

--- app/data/data.tsx
+++ app/data/data.tsx
@@ -5,4 +5,29 @@ const social_media_links = {
     whatsapp: ""
 }
 
-export  {social_media_links } ;
+const aboutMe = {
+    name : "Sankalpa Bandara",
+    background : "With over 4 years of experience in drone piloting, I’m a licensed and certified operator offering tailored solutions for aerial photography, mapping, and inspections. My passion for capturing the world from above drives every project I take on, ensuring professional and stunning results.",
+    card : [
+        {
+            sort: 1,
+            title : "Experience",
+            description : "With over 4 years of experience in drone piloting, I’m a licensed and certified operator offering tailored solutions for aerial photography, mapping, and inspections.",
+            svg: "drone_remote.svg"
+        },
+        {
+            sort: 2,
+            title : "4K Quality",
+            description : "I use the latest drone technology and software to provide high-quality aerial images and videos that meet your project’s needs.",
+            svg: "icon_4k.svg"
+        },
+        {
+            sort: 3,
+            title : "Customization",
+            description : "I work closely with you to understand your project requirements and deliver customized solutions that exceed your expectations.",
+            svg: "drone_icon.svg"
+        }
+    ]
+}
+
+export  {social_media_links, aboutMe}  ;

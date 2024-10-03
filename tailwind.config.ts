1f00cd8c8a748091c12ec5a4f061cf040132eceb
Harshana Batagalla <67497499+harshanabatagalla@users.noreply.github.com>
2024-10-03 12:49:48 +0530

    added new animations

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -16,9 +16,19 @@ const config: Config = {
           '0%': { opacity: '0', transform: 'translateY(40px)' },
           '100%': { opacity: '1', transform: 'translateY(0)' },
         },
+        slideInFromRight: {
+          '0%': { opacity: '0', transform: 'translateX(40px)' },
+          '100%': { opacity: '1', transform: 'translateX(0)' },
+        },
+        slideInFromBottom: {
+          '0%': { opacity: '0', transform: 'translateY(200px)' },
+          '100%': { opacity: '1', transform: 'translateY(0)' },
+        },
       },
       animation: {
         fadeInUp: 'fadeInUp 0.8s 0.5s ease-out both',
+        slideInFromRight: 'slideInFromRight 0.8s 0.5s ease-out forwards',
+        slideInFromBottom: 'slideInFromBottom 0.6s ease-out forwards',
       },
     },
   },
